3fd34da9659ddfb36fb73101482f160d2b14b1cb
Bersder3000 <B1747322151@outlook.com>
2020-07-31 00:44:25 +0800

    aplly markdown css to Link and About Page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -11,10 +11,10 @@
     <div class="page-content-wrap">
       <div class="page-content about fc">
         <div class="content-area">
-          <blockquote>
-            <p>自分の世界を変えるのは自分。 —— モルガナ</p>
-          </blockquote>
-          <div class="about-post">
+          <article class="post-about markdown-area tl">
+            <blockquote>
+              <p>自分の世界を変えるのは自分。 —— モルガナ</p>
+            </blockquote>
             <h2><span>壹</span>关于我</h2>
             <p>因为单纯的喜欢物语系列和猫，所以起了个 <span><ruby>忍野<rp>(</rp><rt>おしの</rt><rp>)</rp></ruby><ruby>喵<rp>(</rp><rt>ニャ</rt><rp>)</rp></ruby></span> 的名字</p>
             <p>在广州上学的一名普通的独身大学生，与所有普通的独身大学生一样普通，吉良吉影都为我点赞</p>
@@ -22,16 +22,12 @@
             <p>jo厨、物语厨、月厨、车万厨</p>
             <p>不善言辞，完美主义</p>
             <p><strong>平凡人，亚洲人，底层民众</strong></p>
-          </div>
-          <div class="about-post">
             <h2><span>貳</span>一些喜好</h2>
             <p>喝茶、听音乐</p>
             <p>看科普、Van 游戏</p>
             <p>宿舍做饭/研究食谱<del>（穷是原罪）</del></p>
             <p>为自己的兴趣瞎折腾</p>
             <p>··· ···</p>
-          </div>
-          <div class="about-post">
             <h2><span>叁</span>关于破站</h2>
             <p>能在茫茫网络中找到我这个破站，真是缘，妙不可言啊。很高兴你能看到这里</p>
             <p>破站用于记录和分享技术文章、日常琐事、吐槽及一些乱七八糟的东西。主要是为了单纯地记录来防止失忆，随缘扩散</p>
@@ -41,7 +37,7 @@
             <div class="history">
               <p v-for="item in siteHistory" :key="item[0]"><time>{{item[0]}}</time><span class="isolate">·</span>{{item[1]}}</p>
             </div>
-          </div>
+          </article>
         </div>
       </div>
     </div>
--- pages/links/index.vue
+++ pages/links/index.vue
@@ -14,20 +14,20 @@
     <div class="page-content-wrap">
       <div class="page-content link">
         <div class="content-area fc">
-          <div class="link-description tl">
+          <div class="link-description markdown-area tl">
             <h2>壹 · 关于友链</h2>
             <p>作为半个肥宅 + 半个技术宅，每访问一个船新的博客，我总会不自觉地去点开 关于&链接，想着会不会又见到一个技术dalao或者和我志趣相投的人。这是一种非常神奇的体验，遇到同类、发现新事物让我欣喜雀跃（或许也只是我的怪癖而已）。</p>
             <p>不管怎样，现在拥有了自己博客之后，我也能参与到这条链的传递当中了，请多多指教</p>
             <del>（虽然我对SPA的SEO不抱什么希望）</del>
             <h2>貳 · 几点希望</h2>
             <ul>
-              <li>符合社会主义核心价值观，网站不会给人造成困扰</li>
+              <li>网站不会给人造成困扰</li>
               <li>全站启用 HTTPS 访问 or 支持HTTP/2</li>
               <li>原创内容过半且定期更新</li>
               <li>第一点是硬性要求，不满足直接否决</li>
             </ul>
             <h2>叁 · 交易方式</h2>
-            <p>通过评论 OR <a href="mailto:oshinoNya@outlook.com">邮箱</a>留下 <strong>名称，链接，简介，头像</strong> 即可如</p>
+            <p>通过 <a href="#comments">评论</a> OR <a href="mailto:oshinoNya@outlook.com">邮箱</a> 留下 <strong>名称，链接，简介，头像</strong> 即可如</p>
             <blockquote>
               <p>Name：忍野喵<br>Link：https://www.bilibili.com<br>Bio：今天喝了典明粥了吗<br>Avatar：https://www.bilibili.com</p>
             </blockquote>
